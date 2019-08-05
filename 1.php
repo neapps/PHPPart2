f24f0e5edad81e4be39e2804f734a5c4e1049f6c
Atrisa Endya N H <30512312+cookie1599@users.noreply.github.com>
2019-08-05 11:43:49 +0700

    mati_lampu

---
+++ 1.php
@@ -0,0 +1,20 @@
+<?php
+// ============
+// OOP pada PHP
+// ============
+// OOP atau Object Oriented Programming adalah 
+// metode pemograman yang lebih berorientasi pada objek
+// atau pemograman yang lebih berpusat terhadap objek.
+// sebenarnya OOP lebih di dukung pada pemograman
+// JAVA dan C++, tetapi di PHP sudah sangant di 
+// dukung pada PHP5.
+// berikut adalah penggunaan metode pada OOP dasar:
+
+// 1. Class adalah sebuah konsep oop yang digunakan
+// untuk membungkus data atau abstraksi prosedural atau
+// library untuk menyimpan property dan method
+
+// 2. Property adalah   
+
+
+?>
